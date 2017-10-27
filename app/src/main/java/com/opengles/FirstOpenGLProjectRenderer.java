ce5503fdaf6497a446f069d0ce77c48dc80bc5ff
Ambitionman868 <497231226@qq.com>
2017-10-27 10:36:28 +0800

    Initial commit

---
+++ app/src/main/java/com/opengles/FirstOpenGLProjectRenderer.java
@@ -0,0 +1,31 @@
+package com.opengles;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by Jianpan on 2017/10/27.
+ */
+
+public class FirstOpenGLProjectRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        //设置清空屏幕用的颜色
+        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        //设置视图尺寸
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        //擦除屏幕上的所有颜色并用之前glClearColor()调用定义的颜色填充整个屏幕
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
---
+++ app/src/main/java/com/opengles/MainActivity.java
@@ -0,0 +1,56 @@
+package com.opengles;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGLSurfaceView;
+    //记住GlSurfaceView是否处于有效状态
+    private boolean mRendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mGLSurfaceView = new GLSurfaceView(this);
+        if (isSupportsEs2()) {
+            mGLSurfaceView.setEGLContextClientVersion(2);
+            mGLSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+            mRendererSet = true;
+        } else {
+            Toast.makeText(this, "不支持OpenGL ES 2.0", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        setContentView(mGLSurfaceView);
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mRendererSet) {
+            mGLSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRendererSet) {
+            mGLSurfaceView.onResume();
+        }
+    }
+
+    private boolean isSupportsEs2() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        return configurationInfo.reqGlEsVersion >= 0x20000;
+    }
+}
